4a315350ece8a7abac029f993a27d60058340d6c
Gianbdf <gian.defaria@hotmail.com>
2023-01-12 21:15:30 -0300

    criando o main

---
+++ src/components/main.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Main() {
+
+    return(
+        <>
+            <header>
+            </header>
+            <main>
+                
+            </main>
+        </>
+    )
+
+}
