79193d75c0b0bf0915ef83d80884061e3ae75689
Gianbdf <gian.defaria@hotmail.com>
2023-01-12 21:27:20 -0300

    add título no cabeçalho

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -5,9 +5,10 @@ export default function Main() {
     return(
         <>
             <header>
+                <h1>Where in the world?</h1>
             </header>
             <main>
-                
+
             </main>
         </>
     )
