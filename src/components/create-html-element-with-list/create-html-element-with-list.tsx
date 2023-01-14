a01d180fa179addcc2c501b7189a31751ea9e905
Gianbdf <gian.defaria@hotmail.com>
2023-01-14 01:18:32 -0300

    esboço inicial do innerhtml de cada lista

--- src/components/create-html-element-with-list/create-html-element-with-list.tsx
+++ src/components/create-html-element-with-list/create-html-element-with-list.tsx
@@ -3,7 +3,21 @@ import { countryList } from "../main/main";
 
 export function createList() {
     const li = document.createElement('li');
-    li.innerHTML = 'teste2'
+    li.className = "country-box"
+    li.innerHTML = 
+    `
+        <div class='flag'>
+            <img src="" alt="flag" />
+        </div>
+        <div class='country-information'>
+            <h1 class='country-name'></h1>
+            <h2>Population:</h2> <p class='population'></p>
+            <h2>Region:</h2> <p class='region'></p>
+            <h2>Capital:</h2> <p class='capital'></p>
+            
+        </div>
+    
+    `
     countryList.appendChild(li);
 
     
