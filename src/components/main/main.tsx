a346a7f330cf1dc849dd4b1145e54ba9c3bf9fd4
Gianbdf <gian.defaria@hotmail.com>
2023-01-15 15:35:59 -0300

    função não utilizada

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,5 +1,5 @@
 import './main.css'
-import { listenerSelectRegionsFilter, ReturnApiCountry, ReturnCountryPerRegion } from '../input-country-list/input-country-list'
+import { listenerSelectRegionsFilter, ReturnApiCountry } from '../input-country-list/input-country-list'
 import { useEffect } from 'react';
 
 export let countryList: HTMLElement;
