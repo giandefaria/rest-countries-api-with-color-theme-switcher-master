e3029fc89496a8d5bb909d8943d80d09f4aa8ed3
Gianbdf <gian.defaria@hotmail.com>
2023-01-18 00:08:50 -0300

    explicando códigos de roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom' //instalo npm install react-router-dom. Importo essas duas funções para adicionar as rotas
 
-import Main from './components/main/main';
-import CountryInfo from './components/country-info/country-info';
+import Main from './components/main/main'; //pagina principal do react. Vou atrelar ao caminho '/' que significa principal
+import CountryInfo from './components/country-info/country-info'; //pagina com as informações de cada país. Vou atrelar ao caminho 'countryinfo'
 
 
 
 
 function App() {
   return (
-    <div className="App">
+    //adiciono as rotas dentro do routes. Indico o path (caminho no endereço html) e qual página (element) será executada
+    <div className="App"> 
       <Routes>
           <Route path='/' element={ <Main />} />
           <Route path='/countryinfo' element={ <CountryInfo />} />
