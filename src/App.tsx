894fd634add407043bfdaeaefb98f24cc23edc69
Gianbdf <gian.defaria@hotmail.com>
2023-01-18 00:04:08 -0300

    caminho countryinfo

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Routes>
           <Route path='/' element={ <Main />} />
-          <Route path='/' element={ <CountryInfo />} />
+          <Route path='/countryinfo' element={ <CountryInfo />} />
       </Routes> 
     </div>
   );
