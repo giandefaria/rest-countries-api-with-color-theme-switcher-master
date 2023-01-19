3433543c24239a6de5e5001858c8c54ad0a8a8c7
Gianbdf <gian.defaria@hotmail.com>
2023-01-18 23:07:23 -0300

    caputrando link da bandeira e atribuindo na variável flag

--- src/components/country-info/country-info.tsx
+++ src/components/country-info/country-info.tsx
@@ -1,18 +1,21 @@
 import { Link, useParams } from "react-router-dom"; 
 
+let flag;
+
 //const CountryInfo será uma função assíncrona que buscará na api o nome do país que está sendo buscado
 //esse nome será informado como parâmetro da função 
 const CountryInfos = async (name: string) => {
     const respNameCountry = await fetch(`https://restcountries.com/v3.1/name/${name}`);
     const respNameCountryJson = await respNameCountry.json(); //a resposta retornada organizo em json
     console.log(respNameCountryJson);
+    flag = respNameCountryJson[0].flags.png;
 } 
 
 export const CountryInfo = (e: any) => {
     const { country } = useParams(); //hook para importar a id do link, que no caso será o nome do país
     //o nome dentro das chaves tem que ser igual ao indicado na rota, que no caso é :country
     CountryInfos(country as string); //executo a busca na api pelo nome do país constante na url
-   
+       
     return (
         <>
             <p>teste - { country }</p>
@@ -40,7 +43,7 @@ export const CountryInfo = (e: any) => {
                         </ul>
                         <ul>
                             <h2>Border Countries:</h2>
-                            
+
                         </ul>
 
                     </div>
