cc1700dbc32c48b84e1c846ca97affef966f48aa
Gianbdf <gian.defaria@hotmail.com>
2023-01-19 20:41:48 -0300

    adicionando useState

--- src/components/country-info/country-info.tsx
+++ src/components/country-info/country-info.tsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom"; 
 
-let flag: string ;
+//let flag: string ;
+//let ativador = false;
 //vou ter que usar o useState e o {xx && ()} para renderizar só após o retorno da api
 
 //const CountryInfo será uma função assíncrona que buscará na api o nome do país que está sendo buscado
 //esse nome será informado como parâmetro da função 
-const CountryInfos = async (name: string) => {
+/*const CountryInfos = async (name: string) => {
     const respNameCountry = await fetch(`https://restcountries.com/v3.1/name/${name}`);
     const respNameCountryJson = await respNameCountry.json(); //a resposta retornada organizo em json
     console.log(respNameCountryJson);
     flag = respNameCountryJson[0].flags.png;
     console.log(flag)
+    ativador = true;
 } 
+*/
 
 export const CountryInfo = (e: any) => {
     const { country } = useParams(); //hook para importar a id do link, que no caso será o nome do país
+    const { flag, setFlag } = useState();
     //o nome dentro das chaves tem que ser igual ao indicado na rota, que no caso é :country
-    CountryInfos(country as string); //executo a busca na api pelo nome do país constante na url
+    //CountryInfos(country as string); //executo a busca na api pelo nome do país constante na url
        
     return (
         <>
@@ -29,7 +34,7 @@ export const CountryInfo = (e: any) => {
                 <button><Link to="/">Back</Link></button>
                 <section>
                     <div className="flag-info">
-                        <img src={flag} alt="flag"/>
+                        {ativador && (<img src={flag} alt="flag"/>)}
                     </div>
                     <div className="information-county">
                         <h1></h1>
