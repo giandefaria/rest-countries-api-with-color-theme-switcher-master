164c08857ae49aaab235eed172c61459522e88a2
Gianbdf <gian.defaria@hotmail.com>
2023-01-20 01:55:06 -0300

    Alterado a versão buscada na api, useState native name e renderização do retorno na página

--- src/components/country-info/country-info.tsx
+++ src/components/country-info/country-info.tsx
@@ -20,17 +20,19 @@ import { Link, useParams } from "react-router-dom";
 export const CountryInfo = (e: any) => {
     const { country } = useParams(); //hook para importar a id do link, que no caso será o nome do país
     const [ flag, setFlag ] = useState();
+    const [ nativeName, setNativeName ] = useState();
     //o nome dentro das chaves tem que ser igual ao indicado na rota, que no caso é :country
     //CountryInfos(country as string); //executo a busca na api pelo nome do país constante na url
     useEffect(() => {
         //const CountryInfo será uma função assíncrona que buscará na api o nome do país que foi clicado
         //o nome do país é que foi importado pelo useParams;
         const CountryInfos = async () => {
-            const respNameCountry = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+            const respNameCountry = await fetch(`https://restcountries.com/v2/name/${country}`);
             const respNameCountryJson = await respNameCountry.json(); //a resposta retornada organizo em json
             console.log(respNameCountryJson);
             setFlag(respNameCountryJson[0].flags.png);
-            console.log(flag)
+            setNativeName(respNameCountryJson[0].nativeName);
+            console.log(flag);
         }
         CountryInfos();
 
@@ -54,7 +56,7 @@ export const CountryInfo = (e: any) => {
                     <div className="information-county">
                         <h1></h1>
                         <ul>
-                            <li></li>
+                            <li><h4>Native Name: </h4> <p>{nativeName}</p></li>
                             <li></li>
                             <li></li>
                             <li></li>
