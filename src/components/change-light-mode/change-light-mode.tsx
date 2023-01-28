4e31cf116e7baa77c2bfd245a4e3d71db4c15154
Gianbdf <gian.defaria@hotmail.com>
2023-01-28 10:49:23 -0300

    adicionando as cores variáveis do main da página

--- src/components/change-light-mode/change-light-mode.tsx
+++ src/components/change-light-mode/change-light-mode.tsx
@@ -1,10 +1,10 @@
-const colors = ['hsl(0, 0%, 98%)', 'black' ];
+const Maincolors = ['hsl(0, 0%, 98%)', 'hsl(207, 26%, 17%)' ];
 
 let index = 0;
 
 export function changeLightMode () {
     index = (index + 1) % 2; //operador módulo binário
     console.log(index);
-    document.documentElement.style.setProperty ('--main-color', colors[index]);
+    document.documentElement.style.setProperty ('--main-color', Maincolors[index]);
 
 }
