f28bbef46559436d4276b94c4576f95f7a5c0c35
Gianbdf <gian.defaria@hotmail.com>
2023-01-28 10:55:43 -0300

    adicionando a variável no country-list

--- src/components/change-light-mode/change-light-mode.tsx
+++ src/components/change-light-mode/change-light-mode.tsx
@@ -1,10 +1,13 @@
-const Maincolors = ['hsl(0, 0%, 98%)', 'hsl(207, 26%, 17%)' ];
+const MainColors = ['hsl(0, 0%, 98%)', 'hsl(207, 26%, 17%)' ];
+const CountryListColorsElements = ['hsl(0, 0%, 100%)', 'hsl(209, 23%, 22%)']
 
 let index = 0;
 
 export function changeLightMode () {
     index = (index + 1) % 2; //operador módulo binário
     console.log(index);
-    document.documentElement.style.setProperty ('--main-color', Maincolors[index]);
+    document.documentElement.style.setProperty ('--main-color', MainColors[index]);
+    document.documentElement.style.setProperty ('--country-list-color', CountryListColorsElements[index]);
+
 
 }
