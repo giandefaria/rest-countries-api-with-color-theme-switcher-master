65fc34a82d3d72ef80ff8089c5fabb03451b5aa4
Gianbdf <gian.defaria@hotmail.com>
2023-01-28 10:27:23 -0300

    criando root global

--- src/components/change-light-mode/change-light-mode.tsx
+++ src/components/change-light-mode/change-light-mode.tsx
@@ -3,7 +3,7 @@ const colors = ['white', 'gray' ];
 let index = 0;
 
 export function changeLightMode () {
-    index = (index + 1) % 2;
+    index = (index + 1) % 2; //operador módulo binário
     console.log(index);
 
 }
