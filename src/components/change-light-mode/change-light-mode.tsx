771e1b0289a34a80ef452077a89ad4367a75220b
Gianbdf <gian.defaria@hotmail.com>
2023-01-28 10:44:44 -0300

    alterando a cor global do elemento main-color ao clicar no botão dark Mode

--- src/components/change-light-mode/change-light-mode.tsx
+++ src/components/change-light-mode/change-light-mode.tsx
@@ -1,9 +1,10 @@
-const colors = ['white', 'gray' ];
+const colors = ['hsl(0, 0%, 98%)', 'black' ];
 
 let index = 0;
 
 export function changeLightMode () {
     index = (index + 1) % 2; //operador módulo binário
     console.log(index);
+    document.documentElement.style.setProperty ('--main-color', colors[index]);
 
 }
