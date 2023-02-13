16cdca0b7d46491c496f34f425dce3de3d9d3923
Gianbdf <gian.defaria@hotmail.com>
2023-02-13 03:59:51 -0300

    alterando a base de mapa que envia as informações para que todos os países sejam mostrados em ingles

--- src/pages/country-info/country-info.tsx
+++ src/pages/country-info/country-info.tsx
@@ -151,8 +151,8 @@ export const CountryInfo = (e: any) => {
                             (
                                 <MapContainer center={mapLocalization} zoom={4} scrollWheelZoom={true} style={{height: "500px"}}>
                                     <TileLayer
-                                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                        attribution='&copy; <a href="https://carto.com/">carto.com</a> contributors'
+                                        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
                                     />
                                     <Marker position={mapLocalization}>
                                     </Marker>
