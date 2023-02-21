4557e9a0f32a347c252c296f23b031ee35144dd3
Gianbdf <gian.defaria@hotmail.com>
2023-02-20 23:11:33 -0300

    adicionando interface

--- src/interfaces/country-information.tsx
+++ src/interfaces/country-information.tsx
@@ -1,3 +1,14 @@
 export interface CountryInformation {
-    
+    flags: HTMLImageElement;
+    name: string;
+    nativeName: string;
+    population: number;
+    region: string;
+    subregion: string;
+    capital: string;
+    topLevelDomain: Array<string> | string;
+    currencies: Array<any>;
+    languages: any;
+    latlng: Array<number>;
+
 }
